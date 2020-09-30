cfc13e4bad82e6319723d9d7c62b55ed7582266d
Colin Murphy <cmurphytaylor@gmail.com>
2020-09-29 21:22:57 -0400

    .

---
+++ GitHubFollowers/Custom Views/ViewControllers/GFRepoItemVC.swift
@@ -0,0 +1,9 @@
+//
+//  GFRepoItemVC.swift
+//  GitHubFollowers
+//
+//  Created by Colin Murphy on 6/30/20.
+//  Copyright © 2020 Colin Murphy. All rights reserved.
+//
+
+import Foundation
---
+++ GitHubFollowers/FollowerListVC.swift
@@ -0,0 +1,30 @@
+//
+//  FollowerListVC.swift
+//  GitHubFollowers
+//
+//  Created by Colin Murphy on 6/29/20.
+//  Copyright © 2020 Colin Murphy. All rights reserved.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
